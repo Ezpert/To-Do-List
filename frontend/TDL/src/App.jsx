8eec41913ee766784d1e66411a068f9d538c7396
Ezpert <109883612+Ezpert@users.noreply.github.com>
2024-11-08 21:46:24 -0600

    Finished Register Page Frontend :)

--- frontend/TDL/src/App.jsx
+++ frontend/TDL/src/App.jsx
@@ -114,7 +114,7 @@ function App() {
         </div>
       )}
       {/* Render Register component when current page is 'Register' */}
-      {currentPage === 'Register' && <Register />}
+      {currentPage === 'Register' && <Register setPage={setCurrentPage} changeVisibility={setIsRegisterButtonVisible}/>}
       {/* Render TaskPage component when current page is 'Tasks' */}
       {currentPage === 'Tasks' && <TaskPage username={username} />}
     </div>
--- frontend/TDL/src/Register.jsx
+++ frontend/TDL/src/Register.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import eyeOpen from './assets/eyeopen.svg';
 import eyeClosed from './assets/eyeclosed.svg';
 import Header from './Header';
+import videoFile from './assets/sign-in-video.mp4'
 
 // Changed to functional component
 function Register(props) {
@@ -37,52 +38,85 @@ function Register(props) {
         }));
     };
 
+
+
     return (
-        <div className='register-container '>
+        <div className="register-page-main-container">
+            <div className='register-container '>
 
-            {postResponse && (
-                <p>POST Response: {JSON.stringify(postResponse)}</p>
-            )}
+                {postResponse && (
+                    <p>POST Response: {JSON.stringify(postResponse)}</p>
+                )}
+                <h1 className="sign-up-text">Sign Up</h1>
+                <Header/>
+                <hr className="divider"></hr>
+                <div className='form-div'>
+                    <form onSubmit={handleFormSubmit} className="registration-form">
+                        <div id="username-div">
+                            <label className="label-username">Username</label>
+                            <input
+                                className="register-username"
+                                type="text"
+                                placeholder="Enter your username..."
+                                value={postFormData.username || ''}
+                                onChange={(e) => updateData({username: e.target.value})}
+                            />
 
-            <Header/>
-            <div className='form-div'>
-                <form onSubmit={handleFormSubmit} className="registration-form">
-                    <div id="username-div">
-                        <label className="label-username">Email</label>
-                        <input
-                            className = "register-username"
-                            type="text"
-                            placeholder="Enter your username..."
-                            value={postFormData.username || ''}
-                            onChange={(e) => updateData({username: e.target.value})}
-                        />
+                        </div>
 
+                        <div id="password-div">
+                            <label className="label-password">Password</label>
+                            <input
+                                className="register-password"
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Enter your password..."
+                                value={postFormData.password || ''}
+                                onChange={(e) => updateData({password: e.target.value})}
+                            />
+
+                            <button
+                                type="button"
+                                className="password-toggle"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <img src={eyeOpen} alt="Todoist Icon"/> :
+                                    <img src={eyeClosed} alt="Todoist Icon"/>}
+                            </button>
+
+                        </div>
+                        <button className="register-submit" type="submit">Sign up with Username</button>
+                    </form>
+                    <div className="text-login-redirect-div">
+                        Already signed up? <a
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault();  // Prevents default link behavior
+                            props.setPage('Landing');
+                            props.changeVisibility(true);
+                        }}
+                        className="anchor-text-login"
+                    >
+                        Go to Login
+                    </a>
                     </div>
+                </div>
+            </div>
+            <div className="video-example-container">
+                <video
+                    src={videoFile}
+                    width="750"
+                    height="500"
+                    autoPlay
+                    muted
+                    playsInline
+                    poster="./assets/todoist.svg" // shows this image before video plays
+                />
+                <div className="sticky-note-div">
+                    <p className="confession-para">Before Patolist, my to-do lists were scattered all around! Now,
+                        everything is in order and in one place.</p>
+                    <p className="confession-user-para">- Armando G.</p>
+                </div>
 
-                    <div id="password-div">
-                        <label className="label-password">Password</label>
-                        <input
-                            className = "register-password"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Enter your password..."
-                            value={postFormData.password || ''}
-                            onChange={(e) => updateData({password: e.target.value})}
-                        />
-
-                        <button
-                            type="button"
-                            className="password-toggle"
-                            onClick={() => setShowPassword(!showPassword)}
-                        >
-                            {showPassword ? <img src={eyeOpen} alt="Todoist Icon"/> :
-                                            <img src={eyeClosed} alt="Todoist Icon"/>}
-                        </button>
-
-                    </div>
-
-
-                    <button className="register-submit"type="submit">Submit</button>
-            </form>
             </div>
         </div>
     );
